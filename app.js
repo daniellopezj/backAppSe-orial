cedf9e3121c24498aec005edd3021b98c5e7b81b
daniel hizo un comentario <daniellopezj0327@gmail.com>
2019-12-16 08:24:14 -0500

    cambios en el acceso principal, retomar acceso principal a app.js

--- app.js
+++ app.js
@@ -1,37 +1,28 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var http = require('http').Server(app);
+
 var routes = require('./routes/router');
 
-
-//MENSAJES CABEZERA A CADA SERVICIO
 var allowCrossDomain = function(req, res, next) {
+    req.header('Access-Control-Allow-Origin', "*");
+    req.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 }
 
-//Control CORS
 app.use(allowCrossDomain);
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 
+routes.assignRoutes(app, http);
 
-// DONDE ESCUCHA EL SERVIDOR
-app.listen(3000, function() {
-    console.log('app listening on port 3000!');
-});
-
-//hacer llamado a enrutador
-routes.assignRoutes(app);
-
-
-//MANEJO DE ERRORES 
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Se presento un problema!');
-});
-
-module.exports = app;
+http.listen(3000, () => {
+    console.log('started')
+})
