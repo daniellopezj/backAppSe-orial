8f55a1f33b00066c10925cdd1652e60d0d4e66ff
daniel hizo un comentario <daniellopezj0327@gmail.com>
2020-02-27 12:40:53 -0500

    cambios

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,3 +1,10 @@
+var body_parser = require('body-parser');
+const UserService = require('../routes/userservice.js')
+const User = require('../routes/user')
+const Person = require('../routes/person.js')
+const TypeService = require('../routes/typeservice')
+const CleanService = require('../routes/cleanservice.js')
+
 exports.assignRoutes = function(app, http) {
 
     app.use(body_parser.urlencoded({ extended: true }));
